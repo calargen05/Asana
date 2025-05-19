e775c0877db675bba2c925f7db38cdaabd5464de
Colin L <eternalnova685@gmail.com>
2025-05-19 13:27:08 -0400

    Add project files.

---
+++ AsanaCLI/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Asana.CLI.Models.ToDo;
+
+namespace Asana.CLI.Models.Project
+{
+    public class Project
+    {
+        private string? id {  get; set; }
+        private string? name { get; set; }
+        private string? description { get; set; }
+        private double? CompletePercent { get; set; }
+        private ToDo
+    }
+}
---
+++ AsanaCLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Asana.CLI.Models.Project;
+
+namespace Asana.CLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+ 
